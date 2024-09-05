7e6fbeb735fb1324c335ab5fbc859aa2079e4dfe
ironquillo24 <138108449+ironquillo24@users.noreply.github.com>
2024-09-05 10:25:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import 
+from snowflake.snowpark.functions import col
 import requests
 
 # Write directly to the app
